6d780766118348549c7fbb8557789d5e73815443
DivinK <kdivin@icloud.com>
2023-12-01 19:44:51 +0100

    Initial Commit

---
+++ Twesehamwe/TwesehamweApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwesehamweApp.swift
+//  Twesehamwe
+//
+//  Created by Divin Karagira on 2023-12-01.
+//
+
+import SwiftUI
+
+@main
+struct TwesehamweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
